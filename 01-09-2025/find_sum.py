5d58e25637521c36776be6abf4e62de1ff00bdee
Varshini E S (vaes) <vaes@cisco.com>
2025-09-01 10:55:59 +0530

    msg

---
+++ 01-09-2025/find_sum.py
@@ -0,0 +1,12 @@
+#find_sum
+'''
+find_sum
+args:firstNum,secondNum
+returns sum of the numbers
+'''
+def find_sum(firstNum,secondNum):
+    result=firstNum+secondNum
+    return result
+
+print(find_sum(10,20))
+print(find_sum(20,30))
